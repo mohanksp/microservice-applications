c72f962cd908de9381f946686db7c123d1b2d818
mohanksp <ksp.mohan@live.in>
2022-02-26 15:38:25 +0530

    create customer service

---
+++ distributed-banking/customer-service/src/main/java/com/devtechnical/customerservice/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package com.devtechnical.customerservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/status/check")
+    public String getStatus(){
+        return "working on port : "+environment.getProperty("server.port");
+    }
+
+    @GetMapping("/{id}")
+    public String getCustomers(@PathVariable String id){
+        return "hello customer service "+id;
+    }
+
+}
