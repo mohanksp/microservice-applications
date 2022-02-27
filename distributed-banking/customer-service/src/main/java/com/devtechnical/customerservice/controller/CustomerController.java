61a984a7012497c0373664c750c76a339953dbff
mohanksp <ksp.mohan@live.in>
2022-02-27 15:30:05 +0530

    create central config-server for manage properties

--- distributed-banking/customer-service/src/main/java/com/devtechnical/customerservice/controller/CustomerController.java
+++ distributed-banking/customer-service/src/main/java/com/devtechnical/customerservice/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package com.devtechnical.customerservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,13 +16,17 @@ public class CustomerController {
     @Autowired
     private Environment environment;
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/status/check")
     public String getStatus(){
+        logger.info("called getStatus() method");
         return "working on port : "+environment.getProperty("local.server.port");
     }
 
     @GetMapping("/{id}")
-    public String getCustomers(@PathVariable String id){
+    public String getCustomers(@PathVariable String id) {
+        logger.info("called getCustomers() method with "+id+" parameter");
         return "hello customer service "+id;
     }
 
