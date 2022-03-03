9d9cf9adccbbc1cf3cb72f860878e77e87de6fa3
mohanksp <ksp.mohan@live.in>
2022-03-03 09:47:51 +0530

    Refactoring config-server and customer-service.

--- distributed-banking/customer-service/src/main/java/com/devtechnical/customerservice/model/Customer.java
+++ distributed-banking/customer-service/src/main/java/com/devtechnical/customerservice/model/Customer.java
@@ -7,10 +7,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Data;
 
 /**
  * @author Mohanksp
  */
+@Data
 @Entity
 public class Customer {
 
@@ -24,83 +26,4 @@ public class Customer {
   private String email;
   private Long mobile;
   private Date creationDate;
-
-  public Customer() {
-  }
-
-  public Customer(Long id, String loginId, String password, String name, String address,
-      String email, Long mobile, Date creationDate) {
-    this.id = id;
-    this.loginId = loginId;
-    this.password = password;
-    this.name = name;
-    this.address = address;
-    this.email = email;
-    this.mobile = mobile;
-    this.creationDate = creationDate;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getLoginId() {
-    return loginId;
-  }
-
-  public void setLoginId(String loginId) {
-    this.loginId = loginId;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getAddress() {
-    return address;
-  }
-
-  public void setAddress(String address) {
-    this.address = address;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public Long getMobile() {
-    return mobile;
-  }
-
-  public void setMobile(Long mobile) {
-    this.mobile = mobile;
-  }
-
-  public Date getCreationDate() {
-    return creationDate;
-  }
-
-  public void setCreationDate(Date creationDate) {
-    this.creationDate = creationDate;
-  }
 }
